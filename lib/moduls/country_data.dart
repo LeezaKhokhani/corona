9f202967b4696de2906d088a1df5c5a90f731c36
khokhanileeza@gmai.com <leeza6234>
2023-04-06 10:37:23 +0530

    updated

---
+++ lib/moduls/country_data.dart
@@ -0,0 +1,23 @@
+class CountryData {
+  final dynamic country;
+  final dynamic infected;
+  final dynamic death;
+  final dynamic recovered;
+  final dynamic population;
+
+  CountryData(
+      {required this.country,
+        required this.infected,
+        required this.death,
+        required this.recovered,
+        required this.population});
+
+  factory CountryData.fromMap({required Map e, required String keyname}) {
+    return CountryData(
+        country: e[keyname]["All"]["country"],
+        infected: e[keyname]["All"]["confirmed"],
+        death: e[keyname]["All"]["deaths"],
+        recovered: e[keyname]["All"]["recovered"],
+        population: e[keyname]["All"]["population"]);
+  }
+}
